face23ccbc5fee5f615befe8972d5b021efd114b
umaysoyluer <umaysoyluer@hotmail.com>
2023-03-08 20:03:38 +0300

    codewars js 8kyu 1., 2. and 3. katas

---
+++ 8kyu/8-1.js
@@ -0,0 +1,9 @@
+/**
+ * Question:
+ * Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+ */
+
+function twoCapitals(name) {
+    let nameArray = name.split(" ");
+    return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
