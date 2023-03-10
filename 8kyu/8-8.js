1c661dd460eb8862dadf21e601597d2994f55ec2
umaysoyluer <umaysoyluer@hotmail.com>
2023-03-10 21:15:31 +0300

    "exercise commit"

---
+++ 8kyu/8-8.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * Now you have to write a function that takes an argument and returns the square of it.
+ */
+
+const square = (n) => n * n
---
+++ 8kyu/8-9.js
@@ -0,0 +1,12 @@
+/**
+ * Question:
+ * Messi is a soccer player with goals in three leagues:
+ * LaLiga
+ * Copa del Rey
+ * Champions
+ * Complete the function to return his total number of goals in all three leagues.
+ */
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
