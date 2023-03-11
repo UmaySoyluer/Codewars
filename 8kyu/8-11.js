ac7845e88f7f33a0ce13cc7cae74af01c1a83464
umaysoyluer <umaysoyluer@hotmail.com>
2023-03-11 17:43:54 +0300

    codewars js 8kyu 10., 11. and 12. katas

---
+++ 8kyu/8-11.js
@@ -0,0 +1,19 @@
+/**
+ * Question:
+ * Your task is to create a function that does four basic mathematical operations.
+ */
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            return value1 + value2;
+        case '-':
+            return value1 - value2;
+        case '*':
+            return value1 * value2;
+        case '/':
+            return value1 / value2;
+        default:
+            return 0;
+    }
+}
---
+++ 8kyu/8-12.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * Write a function which converts the input string to uppercase.
+ */
+
+const makeUpperCase = str => str.toUpperCase();
