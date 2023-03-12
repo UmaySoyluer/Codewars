6eb809efe2c17105fd8d2cb39afeb997517933db
umaysoyluer <umaysoyluer@hotmail.com>
2023-03-12 11:56:35 +0300

    codewars js 8kyu 13., 14. and 15. katas

---
+++ 8kyu/8-14.js
@@ -0,0 +1,10 @@
+/**
+ * Question:
+ * Given an array of integers your solution should find the smallest integer.
+ */
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return args.sort((a, b) => a - b)[0];
+    }
+}
