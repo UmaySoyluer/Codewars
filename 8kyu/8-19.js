02268b92c51aa62f21369ec18f09a4a0465659ee
umaysoyluer <umaysoyluer@hotmail.com>
2023-03-15 10:15:34 +0300

    codewars js 8kyu 19., 20. and 21. katas

---
+++ 8kyu/8-19.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+ */
+
+const check = (a,x) => a.includes(x);
