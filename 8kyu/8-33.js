146056ae415bea5126672667438c62a062deb4e9
umaysoyluer <umaysoyluer@hotmail.com>
2023-04-04 13:54:23 +0300

    codewars js 8kyu 33.kata

---
+++ 8kyu/8-33.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+ */
+
+makeNegative = num => num <= 0 ? num : -num;
