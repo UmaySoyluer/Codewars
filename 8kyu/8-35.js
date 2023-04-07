17056dee434968d875af54058f1a7a2e01ea63ac
umaysoyluer <umaysoyluer@hotmail.com>
2023-04-07 12:45:35 +0300

    codewars js 8kyu 35.kata

---
+++ 8kyu/8-35.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+ */
+
+const fakeBin = (x) => x.split('').map(i => i < 5 ? 0 : 1).join('');
