1d55c5fc3ac38f0478cbba06fa64485d0083393e
umaysoyluer <umaysoyluer@hotmail.com>
2023-04-25 16:10:12 +0300

    7kyu js kata commit

---
+++ 7kyu/Between Extremes.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * Given an array of numbers, return the difference between the largest and smallest values.
+ */
+
+const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers);
