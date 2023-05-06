306360f5c8db3e1098ec8f18968754ddd33f163e
umaysoyluer <umaysoyluer@hotmail.com>
2023-05-06 16:22:47 +0300

    codewars js 8kyu 51.kata

---
+++ 8kyu/8-51.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+ */
+
+const isDivisible = (n, x, y) => n % x === 0 && n % y === 0
