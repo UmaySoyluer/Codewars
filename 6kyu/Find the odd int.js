b65ce7d17a5e469e34be6daf2db2194f44a311b3
umaysoyluer <umaysoyluer@hotmail.com>
2023-05-31 15:57:43 +0300

    6kyu js kata commit

---
+++ 6kyu/Find the odd int.js
@@ -0,0 +1,15 @@
+/**
+ * Question:
+ * Given an array of integers, find the one that appears an odd number of times.
+ *
+ * There will always be only one integer that appears an odd number of times.
+ */
+function findOdd(A) {
+    for (let i = 0; i < A.length; i++) {
+        let count = 0;
+        for (let j = 0; j < A.length; j++) {
+            if (A[i] === A[j]) count++;
+        }
+        if (count % 2 !== 0) return A[i]
+    }
+}
