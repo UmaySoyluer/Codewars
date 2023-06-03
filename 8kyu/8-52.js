d67609fe4da1544f7c2dc88b77e717fc7d21fd6b
umaysoyluer <umaysoyluer@hotmail.com>
2023-06-03 16:06:00 +0300

    codewars js 8kyu 52.kata

---
+++ 8kyu/8-52.js
@@ -0,0 +1,6 @@
+/**
+ * Question:
+ * I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+ */
+
+const arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((a, b) => a + b)
