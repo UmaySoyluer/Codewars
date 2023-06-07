d1eba1826ec9f62ac624e64c7a7c016d276a2d1e
umaysoyluer <umaysoyluer@hotmail.com>
2023-06-07 18:27:26 +0300

    7kyu js kata commit

---
+++ 7kyu/Sum - Square Even, Root Odd.js
@@ -0,0 +1,12 @@
+/**
+ * Question:
+ * Complete the function that takes a list of numbers (nums), as the only argument to the function. Take each number in the list and square it if it is even, or square root the number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+ *
+ * The list will never be empty and will only contain values that are greater than or equal to zero.
+ */
+
+const sumSquareEvenRootOdd = ns => {
+    let transformedNumbers = ns.map(i => i % 2 === 0 ? Math.pow(i, 2) : Math.sqrt(i))
+    let sum = transformedNumbers.reduce((a, b) => a + b, 0)
+    return Number(sum.toFixed(2))
+};
