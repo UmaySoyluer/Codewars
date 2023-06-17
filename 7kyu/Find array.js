48afebace49ab7888144338710107e218bd9d8d4
Umay333 <umaysoyluer@hotmail.com>
2023-06-17 14:04:20 +0300

    7kyu js kata commit

---
+++ 7kyu/Find array.js
@@ -0,0 +1,29 @@
+/**
+ * Question:
+ * TYou are given two arrays arr1 and arr2, where arr2 always contains integers.
+ *
+ * Write a function such that:
+ *
+ * For arr1 = ['a', 'a', 'a', 'a', 'a'], arr2 = [2, 4] the function returns ['a', 'a']
+ *
+ * For arr1 = [0, 1, 5, 2, 1, 8, 9, 1, 5], arr2 = [1, 4, 7] the function returns [1, 1, 1]
+ *
+ * For arr1 = [0, 3, 4], arr2 = [2, 6] the function returns [4]
+ *
+ * For arr1=["a","b","c","d"] , arr2=[2,2,2], the function returns ["c","c","c"]
+ *
+ * For arr1=["a","b","c","d"], arr2=[3,0,2] the function returns ["d","a","c"]
+ *
+ * Note that when an element inside arr2 is greater than the index of the last element of arr1 no element of arr1 should be added to the resulting array. If either arr1 or arr2 is empty, you should return an empty arr.
+ */
+
+function findArray(arr1, arr2) {
+    let arr3 = [];
+    if (arr1.length === 0 || arr2.length === 0) {
+        return arr3
+    }
+    for (const i of arr2) {
+        arr3.push(arr1[i])
+    }
+    return arr3
+}
